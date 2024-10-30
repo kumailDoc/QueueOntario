1a4ee6a5dd33b6a40eb2d6376301231057314ac8
zzannat <zzannat@my.centennialcollege.ca>
2024-10-29 22:53:59 -0400

    Adding QueueOntario-Backend project

---
+++ src/main/java/com/group3/project/QueueOntario/QueueOntarioApplication.java
@@ -0,0 +1,13 @@
+package com.group3.project.QueueOntario;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class QueueOntarioApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(QueueOntarioApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/group3/project/QueueOntario/QueueOntarioApplicationTests.java
@@ -0,0 +1,13 @@
+package com.group3.project.QueueOntario;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class QueueOntarioApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
