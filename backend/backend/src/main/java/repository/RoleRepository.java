4b7440f7d595d08e57bdd30c4b17f56734695415
Kumail Doc <dkumail@my.centennialcollege.ca>
2024-10-31 16:19:39 -0400

    Added user registration and signing backend

---
+++ backend/backend/src/main/java/repository/RoleRepository.java
@@ -0,0 +1,12 @@
+package repository;
+
+import java.util.Optional;
+
+import models.ERole;
+import models.Role;
+import org.springframework.data.mongodb.repository.MongoRepository;
+
+
+public interface RoleRepository extends MongoRepository<Role, String> {
+    Optional<Role> findByName(ERole name);
+}
