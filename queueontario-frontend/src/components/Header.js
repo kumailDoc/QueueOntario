9dcec1e510bc9c927d9b375eae91b8ead77e83ed
Kumail Doc <dkumail@my.centennialcollege.ca>
2024-12-08 18:11:11 -0500

    Fixed the profile page button

--- queueontario-frontend/src/components/Header.js
+++ queueontario-frontend/src/components/Header.js
@@ -34,7 +34,7 @@ const Header = () => {
       <nav className="nav-right">
         <ul>
           {userInfo ? (
-            // Display username, profile, and logout buttons if logged in
+            // Display username button and other controls if logged in
             <>
               {userInfo.roles[0] === 'ROLE_ADMIN' && (
                 <li><Link to="/admin" className='adminBtn'>Admin Portal</Link></li>
@@ -43,8 +43,11 @@ const Header = () => {
                 <li><Link to='/mod' className="modBtn">Mod Portal</Link></li>
               )}
               <li><button onClick={handleCheckWaitlist} className="cwaitlist-button">Check Waitlist</button></li>
-              <li className='greeting'>Welcome, {userInfo.username}</li>
-              <li><button onClick={handleProfile} className="profile-button">Profile</button></li>
+              <li>
+                <button onClick={handleProfile} className="user-info">
+                  Welcome, {userInfo.username}
+                </button>
+              </li>
               <li><button onClick={handleLogout} className="logout-button">Log Out</button></li>
             </>
           ) : (
