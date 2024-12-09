fa062acd9dd2de148753b5c17658cc7eb36a6159
Brian Duong <brianduong@hotmail.com>
2024-12-08 22:59:34 -0500

    More css change

--- queueontario-frontend/src/components/Contact.js
+++ queueontario-frontend/src/components/Contact.js
@@ -1,19 +1,23 @@
 import React from 'react';
 import Header from './Header';
 import Footer from './Footer';
-import '../styles/About.css';
+import '../styles/Contact.css';
 
 const Contact = () => {
   return (
-    <div style={{ padding: "20px" }}>
-    <Header />
-      <h1>Contact Us</h1>
-      <p>Feel free to reach out to us through the following methods:</p>
-      <ul>
-        <li><strong>Phone:</strong> +1 (555) 123-4567</li>
-        <li><strong>Email:</strong> contact@example.com</li>
-        <li><strong>Address:</strong> 123 Main St, Toronto, Ontario, Canada</li>
-      </ul>
+    <div className='main-body'>
+      <Header />
+      <div style={{ padding: "20px" }}>
+      
+        <h1>Contact Us</h1>
+        <p>Feel free to reach out to us through the following methods:</p>
+        <ul>
+          <li><strong>Phone:</strong> +1 (555) 123-4567</li>
+          <li><strong>Email:</strong> contact@example.com</li>
+          <li><strong>Address:</strong> 123 Main St, Toronto, Ontario, Canada</li>
+        </ul>
+        
+      </div>
       <Footer />
     </div>
   );
